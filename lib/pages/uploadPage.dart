b06063d6c4ea9eadd49c7702a558291257745731
Izack Tay <isaactaypb@gmail.com>
2022-11-06 23:53:01 +0800

    changed uploadPage

--- lib/pages/uploadPage.dart
+++ lib/pages/uploadPage.dart
@@ -69,76 +69,109 @@ class _UploadPageState extends State<UploadPage> {
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.jpg'),
-            fit: BoxFit.cover,
-          )
-      ),
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.white.withOpacity(0.7),
-        body: ListView(
-          children: [
-            SizedBox(height: h * 0.06,),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {pickImageCamera();},
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.white),
-                      padding: MaterialStateProperty.all(EdgeInsets.all(65))),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xfff6f1e4),
+      body: ListView(
+        children: [
+          SizedBox(height: h * 0.06,),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {pickImageCamera();},
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(65))),
 
-                  child:Icon(Icons.camera_alt, size: 100, color: Colors.orangeAccent),
-                ),
-              ],
-            ),
-
-            SizedBox(height: h * 0.03,),
-            Container(
-              height: h*0.08,
-              margin: const EdgeInsets.only(left: 25, right: 200),
-              width: w*0.95,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 10,
-                        spreadRadius: 7,
-                        offset: Offset(1, 1),
-                        color: Colors.grey.withOpacity(0.2)
-                    )
-                  ]
+                child:Icon(Icons.camera_alt, size: 100, color: Colors.orangeAccent),
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: " Price",
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 1.0
-                        )
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 1.0
-                        )
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)
-                    )
-                ),
+            ],
+          ),
+
+          SizedBox(height: h * 0.03,),
+          Container(
+            height: h*0.08,
+            margin: const EdgeInsets.only(left: 25, right: 200),
+            width: w*0.95,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 7,
+                      offset: Offset(1, 1),
+                      color: Colors.grey.withOpacity(0.2)
+                  )
+                ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: " Price",
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0
+                      )
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0
+                      )
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)
+                  )
               ),
             ),
-            SizedBox(height: h * 0.015,),
-            Container(
-              height: h*0.08,
+          ),
+          SizedBox(height: h * 0.015,),
+          Container(
+            height: h*0.08,
+            margin: const EdgeInsets.only(left: 25, right: 25),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 7,
+                      offset: Offset(1, 1),
+                      color: Colors.grey.withOpacity(0.2)
+                  )
+                ]
+            ),
+            child: TextField(
+              maxLines: 50,
+              decoration: InputDecoration(
+                  hintText: " Product Title",
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0
+                      )
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 1.0
+                      )
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)
+                  )
+              ),
+            ),
+          ),
+          SizedBox(height: h * 0.015,),
+          Flexible(
+            child: Container(
               margin: const EdgeInsets.only(left: 25, right: 25),
               decoration: BoxDecoration(
                   color: Colors.white,
@@ -154,8 +187,9 @@ class _UploadPageState extends State<UploadPage> {
               ),
               child: TextField(
                 maxLines: 50,
+                minLines: 1,
                 decoration: InputDecoration(
-                    hintText: " Product Title",
+                    hintText: " Description",
                     focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
                         borderSide: BorderSide(
@@ -176,69 +210,27 @@ class _UploadPageState extends State<UploadPage> {
                 ),
               ),
             ),
-            SizedBox(height: h * 0.015,),
-            Flexible(
-              child: Container(
-                margin: const EdgeInsets.only(left: 25, right: 25),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 10,
-                          spreadRadius: 7,
-                          offset: Offset(1, 1),
-                          color: Colors.grey.withOpacity(0.2)
-                      )
-                    ]
+          ),
+          SizedBox(height: h * 0.015,),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () { uploadItem("test", "mint condition", "65"); },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                        padding: MaterialStateProperty.all(EdgeInsets.all(13))),
+                    child: Text("Post",style: TextStyle(fontSize: 20, fontStyle: FontStyle.normal),)
+
                 ),
-                child: TextField(
-                  maxLines: 50,
-                  minLines: 1,
-                  decoration: InputDecoration(
-                      hintText: " Description",
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 1.0
-                          )
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 1.0
-                          )
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)
-                      )
-                  ),
-                ),
-              ),
+              ],
             ),
-            SizedBox(height: h * 0.015,),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () { uploadItem("test", "mint condition", "65"); },
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
-                          padding: MaterialStateProperty.all(EdgeInsets.all(13))),
-                      child: Text("Post",style: TextStyle(fontSize: 20, fontStyle: FontStyle.normal),)
-
-                  ),
-                ],
-              ),
-            ),
+          ),
 
 
-          ],
-        ),
+        ],
       ),
     );
   }
