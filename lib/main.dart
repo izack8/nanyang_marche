2d3edfd550811bafcc219266cca09e23e9a38aaf
Izack Tay <isaactaypb@gmail.com>
2022-11-14 00:22:04 +0800

    test to run on iphone

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:nanyang_marche/pages/onboardingPage.dart';
 import 'package:nanyang_marche/pages/loginPage2.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
